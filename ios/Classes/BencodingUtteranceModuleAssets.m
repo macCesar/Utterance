7d0e8e568422e25f34b2b9c53a62f7dadd66d3ac
Cesar Estrada <cesar@codigomovil.mx>
2022-08-23 11:40:30 -0500

    v2.0.0 compiled with M1 Processor and SDK 11

--- ios/Classes/BencodingUtteranceModuleAssets.m
+++ ios/Classes/BencodingUtteranceModuleAssets.m
@@ -1,10 +1,5 @@
 /**
- * Copyright (c) 2013 by Benjamin Bahrenburg. All Rights Reserved.
- * Licensed under the terms of the Apache 2.0 License
- * Please see the LICENSE included with this distribution for details.
- *
- * Available at https://github.com/benbahrenburg/Utterance
- *
+ * This is a generated file. Do not edit or your changes will be lost
  */
 #import "BencodingUtteranceModuleAssets.h"
 
@@ -12,20 +7,18 @@ extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation BencodingUtteranceModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
-	//##TI_AUTOGEN_BEGIN asset
-	//Compiler generates code for asset here
-	return nil; // DEFAULT BEHAVIOR
-	//##TI_AUTOGEN_END asset
+  
+
+  return nil;
 }
 
-- (NSData*) resolveModuleAsset:(NSString*)path
+- (NSData *)resolveModuleAsset:(NSString *)path
 {
-	//##TI_AUTOGEN_BEGIN resolve_asset
-	//Compiler generates code for asset resolution here
-	return nil; // DEFAULT BEHAVIOR
-	//##TI_AUTOGEN_END resolve_asset
+  
+
+  return nil;
 }
 
 @end
